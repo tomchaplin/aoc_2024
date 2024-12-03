a65561c7db74987dd2ad53644edf9da857fa35c0
Thomas Chaplin <tomrchaplin@gmail.com>
2024-12-03 13:18:50 +0000

    Day 1 + 2

--- src/days/day01.rs
+++ src/days/day01.rs
@@ -1,6 +1,54 @@
+use std::collections::HashSet;
+
 use crate::ProblemSolution;
 #[allow(unused_imports)]
 use aoc_parse::{parser, prelude::*};
+use itertools::Itertools;
 pub struct Solution {}
 
-impl ProblemSolution for Solution {}
+fn parse_input<L: Extend<usize> + Default, R: Extend<usize> + Default>(input: &str) -> (L, R) {
+    input
+        .split_whitespace()
+        .chunks(2)
+        .into_iter()
+        .map(|mut chnk| {
+            (
+                chnk.next().unwrap().parse::<usize>().unwrap(),
+                chnk.next().unwrap().parse::<usize>().unwrap(),
+            )
+        })
+        .unzip()
+}
+
+impl ProblemSolution for Solution {
+    fn solve_a(&self, input: &str) -> Option<String> {
+        let (mut left_nums, mut right_nums): (Vec<_>, Vec<_>) = parse_input(input);
+
+        left_nums.sort_unstable();
+        right_nums.sort_unstable();
+
+        // There are no repeated elements in the left list which makes my answer to part b valid
+        for (a, b) in left_nums.iter().tuple_windows() {
+            assert_ne!(a, b);
+        }
+
+        let total_diff: usize = left_nums
+            .into_iter()
+            .zip(right_nums.into_iter())
+            .map(|(l, r)| r.abs_diff(l))
+            .sum();
+
+        Some(total_diff.to_string())
+    }
+
+    fn solve_b(&self, input: &str) -> Option<String> {
+        let (left_nums, right_nums): (HashSet<_>, Vec<_>) = parse_input(input);
+
+        let similarity: usize = right_nums
+            .into_iter()
+            .filter(|n| left_nums.contains(n))
+            .sum();
+
+        Some(similarity.to_string())
+    }
+}
--- src/days/day02.rs
+++ src/days/day02.rs
@@ -1,6 +1,89 @@
 use crate::ProblemSolution;
 #[allow(unused_imports)]
 use aoc_parse::{parser, prelude::*};
+use itertools::Itertools;
 pub struct Solution {}
 
-impl ProblemSolution for Solution {}
+trait SafetyCheck {
+    fn deltas<'a>(&'a self) -> impl Iterator<Item = isize> + 'a;
+
+    fn is_monotonic(&self) -> bool {
+        self.deltas().map(|d| d.signum()).all_equal()
+    }
+
+    fn is_bounded(&self) -> bool {
+        self.deltas().all(|d| d.abs() >= 1 && d.abs() <= 3)
+    }
+
+    fn is_safe(&self) -> bool {
+        self.is_monotonic() && self.is_bounded()
+    }
+}
+
+struct Report(Vec<usize>);
+
+impl SafetyCheck for Report {
+    fn deltas<'a>(&'a self) -> impl Iterator<Item = isize> + 'a {
+        self.0
+            .iter()
+            .tuple_windows()
+            .map(|(a, b)| *a as isize - *b as isize)
+    }
+}
+
+impl Report {
+    fn mask<'a>(&'a self, masked_idx: usize) -> MaskedReport<'a> {
+        MaskedReport {
+            report: self,
+            masked_idx,
+        }
+    }
+
+    fn has_safe_mask(&self) -> bool {
+        (0..self.0.len()).any(|idx| self.mask(idx).is_safe())
+    }
+}
+
+struct MaskedReport<'a> {
+    report: &'a Report,
+    masked_idx: usize,
+}
+
+impl<'r> SafetyCheck for MaskedReport<'r> {
+    fn deltas<'a>(&'a self) -> impl Iterator<Item = isize> + 'a {
+        self.report
+            .0
+            .iter()
+            .enumerate()
+            .filter(|(idx, _num)| *idx != self.masked_idx)
+            .map(|(_idx, num)| num)
+            .tuple_windows()
+            .map(|(a, b)| *a as isize - *b as isize)
+    }
+}
+
+fn parse_input(input: &str) -> Vec<Report> {
+    let p = parser!(
+        lines(
+            rep:repeat_sep(usize, " ") => Report(rep)
+        )
+    );
+    p.parse(input).unwrap()
+}
+
+impl ProblemSolution for Solution {
+    fn solve_a(&self, input: &str) -> Option<String> {
+        let reports = parse_input(input);
+        let n_safe = reports.iter().filter(|rep| rep.is_safe()).count();
+        Some(n_safe.to_string())
+    }
+
+    fn solve_b(&self, input: &str) -> Option<String> {
+        let reports = parse_input(input);
+        let n_safe = reports
+            .iter()
+            .filter(|rep| rep.is_safe() || rep.has_safe_mask())
+            .count();
+        Some(n_safe.to_string())
+    }
+}
