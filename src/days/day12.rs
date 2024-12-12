c41577791a12a7d6fdffc32b6e8c6bbd63cc10cf
Thomas Chaplin <tomrchaplin@gmail.com>
2024-12-12 15:19:11 +0000

    Refactor day 12

--- src/days/day12.rs
+++ src/days/day12.rs
@@ -11,6 +11,95 @@ struct Grid {
     labels: Vec<Vec<char>>,
 }
 
+struct GridPosition<'a> {
+    grid: &'a Grid,
+    position: Position,
+}
+
+impl<'a> GridPosition<'a> {
+    fn label(&self) -> char {
+        self.grid.labels[self.position.0][self.position.1]
+    }
+
+    fn nbrs(&self) -> impl Iterator<Item = GridPosition<'_>> + '_ {
+        let deltas = [(-1, 0), (1, 0), (0, -1), (0, 1)];
+        let pos = (self.position.0 as isize, self.position.1 as isize);
+        deltas
+            .into_iter()
+            .map(move |d| (pos.0 + d.0, pos.1 + d.1))
+            .filter_map(|p| self.grid.try_position(p))
+    }
+
+    fn is_outward(&self, delta: (isize, isize)) -> bool {
+        let posi = (self.position.0 as isize, self.position.1 as isize);
+        let other_pos = (posi.0 + delta.0, posi.1 + delta.1);
+        match self.grid.try_position(other_pos) {
+            // Nbr on grid => check if label matches
+            Some(other) => {
+                if other.label() != self.label() {
+                    true
+                } else {
+                    false
+                }
+            }
+            // Nbr off grid => perimeter
+            None => true,
+        }
+    }
+
+    fn outward_directions(&self) -> impl Iterator<Item = (isize, isize)> + '_ {
+        let deltas = [(-1, 0), (1, 0), (0, -1), (0, 1)];
+        deltas.into_iter().filter(move |d| self.is_outward(*d))
+    }
+
+    fn perimiter_contribution(&self) -> usize {
+        self.outward_directions().count()
+    }
+
+    fn corner_count(&self) -> usize {
+        let posi = (self.position.0 as isize, self.position.1 as isize);
+        self.outward_directions()
+            .filter(|d| {
+                // Given an outward direction (dir)
+                // Move over one position and we should still be in the same region and dir should still be an outward direction
+                // Otherwise we have turned a corner
+                let nbr_direction = rotate(*d);
+                let nbr = (posi.0 + nbr_direction.0, posi.1 + nbr_direction.1);
+                let Some(nbr) = self.grid.try_position(nbr) else {
+                    // Must be a corner direction because we left region
+                    return true;
+                };
+                if nbr.label() != self.label() {
+                    // Must be a corner direction because we left region
+                    return true;
+                }
+                // Final check that direction is still outward (this checks for interior corners)
+                !nbr.is_outward(*d)
+            })
+            .count()
+    }
+
+    fn get_region(&self) -> HashSet<Position> {
+        let mut region = HashSet::new();
+        let mut to_visit = HashSet::new();
+        to_visit.insert(self.position);
+        while let Some(next_pos) = to_visit.iter().next().cloned() {
+            // Remove from to_visit
+            to_visit.remove(&next_pos);
+            // Look for nbrs with same label
+            let next_pos = self.grid.position(next_pos);
+            for nbr in next_pos.nbrs() {
+                if nbr.label() == next_pos.label() && !region.contains(&nbr.position) {
+                    to_visit.insert(nbr.position);
+                }
+            }
+            // Add to the region
+            region.insert(next_pos.position);
+        }
+        region
+    }
+}
+
 fn rotate(delta: (isize, isize)) -> (isize, isize) {
     match delta {
         (1, 0) => (0, -1),
@@ -26,15 +115,18 @@ impl Grid {
         Self { labels }
     }
 
-    fn label(&self, pos: &Position) -> char {
-        self.labels[pos.0][pos.1]
-    }
-
     fn bounds(&self) -> (usize, usize) {
         (self.labels.len(), self.labels[0].len())
     }
 
-    fn try_position(&self, pos: (isize, isize)) -> Option<Position> {
+    fn position(&self, position: Position) -> GridPosition<'_> {
+        GridPosition {
+            grid: self,
+            position,
+        }
+    }
+
+    fn try_position_inner(&self, pos: (isize, isize)) -> Option<Position> {
         let (height, width) = self.bounds();
         if pos.0 < 0 || pos.1 < 0 {
             return None;
@@ -47,86 +139,8 @@ impl Grid {
         }
     }
 
-    fn nbrs<'a>(&'a self, pos: &'a Position) -> impl Iterator<Item = Position> + 'a {
-        let deltas = [(-1, 0), (1, 0), (0, -1), (0, 1)];
-        let pos = (pos.0 as isize, pos.1 as isize);
-        deltas
-            .into_iter()
-            .map(move |d| (pos.0 + d.0, pos.1 + d.1))
-            .filter_map(|p| self.try_position(p))
-    }
-
-    fn is_outward(&self, pos: &Position, delta: (isize, isize)) -> bool {
-        let posi = (pos.0 as isize, pos.1 as isize);
-        let other_pos = (posi.0 + delta.0, posi.1 + delta.1);
-        match self.try_position(other_pos) {
-            // Nbr on grid => check if label matches
-            Some(other) => {
-                if self.label(&other) != self.label(&pos) {
-                    true
-                } else {
-                    false
-                }
-            }
-            // Nbr off grid => perimeter
-            None => true,
-        }
-    }
-
-    fn outward_directions<'a>(
-        &'a self,
-        pos: &'a Position,
-    ) -> impl Iterator<Item = (isize, isize)> + 'a {
-        let deltas = [(-1, 0), (1, 0), (0, -1), (0, 1)];
-        deltas
-            .into_iter()
-            .filter(move |d| self.is_outward(&pos, *d))
-    }
-
-    fn perimiter_contribution(&self, pos: Position) -> usize {
-        self.outward_directions(&pos).count()
-    }
-
-    fn corner_count(&self, pos: &Position) -> usize {
-        let posi = (pos.0 as isize, pos.1 as isize);
-        self.outward_directions(pos)
-            .filter(|d| {
-                // Given an outward direction (dir)
-                // Move over one position and we should still be in the same region and dir should still be an outward direction
-                // Otherwise we have turned a corner
-                let nbr_direction = rotate(*d);
-                let nbr = (posi.0 + nbr_direction.0, posi.1 + nbr_direction.1);
-                let Some(nbr) = self.try_position(nbr) else {
-                    // Must be a corner direction because we left region
-                    return true;
-                };
-                if self.label(&nbr) != self.label(pos) {
-                    // Must be a corner direction because we left region
-                    return true;
-                }
-                // Final check that direction is still outward (this checks for interior corners)
-                !self.is_outward(&nbr, *d)
-            })
-            .count()
-    }
-
-    fn get_region(&self, pos: Position) -> HashSet<Position> {
-        let mut region = HashSet::new();
-        let mut to_visit = HashSet::new();
-        to_visit.insert(pos);
-        while let Some(next_pos) = to_visit.iter().next().cloned() {
-            // Remove from to_visit
-            to_visit.remove(&next_pos);
-            // Look for nbrs with same label
-            for nbr in self.nbrs(&next_pos) {
-                if self.label(&nbr) == self.label(&next_pos) && !region.contains(&nbr) {
-                    to_visit.insert(nbr);
-                }
-            }
-            // Add to the region
-            region.insert(next_pos);
-        }
-        region
+    fn try_position(&self, pos: (isize, isize)) -> Option<GridPosition<'_>> {
+        Some(self.position(self.try_position_inner(pos)?))
     }
 
     fn get_all_regions(&self) -> Vec<HashSet<Position>> {
@@ -136,7 +150,7 @@ impl Grid {
             .flat_map(|i| (0..width).map(move |j| (i, j)))
             .collect();
         while let Some(next_pos) = remaining.iter().next().cloned() {
-            let next_region = self.get_region(next_pos);
+            let next_region = self.position(next_pos).get_region();
             for pos in next_region.iter() {
                 remaining.remove(&pos);
             }
@@ -147,13 +161,19 @@ impl Grid {
 
     fn price_region_a(&self, region: &HashSet<Position>) -> usize {
         let area = region.len();
-        let perim: usize = region.iter().map(|p| self.perimiter_contribution(*p)).sum();
+        let perim: usize = region
+            .iter()
+            .map(|p| self.position(*p).perimiter_contribution())
+            .sum();
         area * perim
     }
 
     fn price_region_b(&self, region: &HashSet<Position>) -> usize {
         let area = region.len();
-        let corners: usize = region.iter().map(|p| self.corner_count(p)).sum();
+        let corners: usize = region
+            .iter()
+            .map(|p| self.position(*p).corner_count())
+            .sum();
         area * corners
     }
 }
